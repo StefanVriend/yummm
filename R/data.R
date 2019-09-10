30ef13f0b872683ec8f6aed913d1af43b72ebb8d
StefanVriend <svriend@gmail.com>
2019-09-10 18:15:07 +0200

    Updata data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Bananas per week
+#'
+#' A dataset containing the average number of bananas eaten per week for 1,000 supermarkt visitors.
+#'
+#' @format A data frame with 1000 observations and 2 variables:
+#' \describe{
+#'   \item{ID}{Personal IDs for supermarkt visitors (1--1000)}
+#'   \item{n}{Average umber of bananas eaten per week (0--10)}
+#' }
+
+"bpw"
