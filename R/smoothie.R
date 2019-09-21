c7abd928d8053d0bae852b12aab442ce8517982c
StefanVriend <svriend@gmail.com>
2019-09-21 15:01:24 +0200

    Add error to smoothie: alpha levels must sum to 1.

--- R/smoothie.R
+++ R/smoothie.R
@@ -23,30 +23,33 @@ smoothie <- function(food, alpha=rep(1/length(food), length(food))) {
     dplyr::pull(Colour) ->
     colours
 
-  colours.rgb <- t(col2rgb(colours))
+  if(sum(alpha) != 1) cat("Error: the alpha levels must sum to 1.")
 
-  if(any(food %in% food.items == FALSE)) {
-    not.in.yummm <- food[!(food %in% food.items)]
+  if(sum(alpha) == 1) {
+    colours.rgb <- t(col2rgb(colours))
 
-    purrr::walk(.x = not.in.yummm,
-                .f = ~{
-                  cat("Error: ", '"', .x,  '"', " is ",
-                      crayon::underline("not"), " part of yummm.\n",
-                      "Find out whether your favourite food ",
-                      "is part of yummm using in.yummm().\n",
-                      sep="")
-                })
+    if(any(food %in% food.items == FALSE)) {
+      not.in.yummm <- food[!(food %in% food.items)]
+
+      purrr::walk(.x = not.in.yummm,
+                  .f = ~{
+                    cat("Error: ", '"', .x,  '"', " is ",
+                        crayon::underline("not"), " part of yummm.\n",
+                        "Find out whether your favourite food ",
+                        "is part of yummm using in.yummm().\n",
+                        sep="")
+                  })
+    }
+
+    if(!any(food %in% food.items == FALSE)) {
+      col2hex(c(ceiling(sum(colours.rgb[,1] * alpha)),
+                ceiling(sum(colours.rgb[,2] * alpha)),
+                ceiling(sum(colours.rgb[,3] * alpha))
+      )) -> smoothie
+
+      return(smoothie)
+    }
   }
-
-  if(!any(food %in% food.items == FALSE)) {
-    col2hex(c(ceiling(sum(colours.rgb[,1] * alpha)),
-              ceiling(sum(colours.rgb[,2] * alpha)),
-              ceiling(sum(colours.rgb[,3] * alpha))
-    )) -> smoothie
-
-    return(smoothie)
-  }
-
 
 }
 
