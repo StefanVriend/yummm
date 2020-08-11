540cb5c00583fd5cc0085aa8a6b90c8c3058672d
Stefan <svriend@gmail.com>
2020-08-11 14:35:23 +0200

    Update market to select a 10-col palette from image
    Market is filled using fill.market, which select 10 colours from image using paletter.

--- R/market.R
+++ R/market.R
@@ -1,6 +1,6 @@
-#' The yummm market: a table of food and palettes
+#' Fill the yummm market with food palettes
 #'
-#' A table with all food and palettes in \code{yummm}.
+#' Fill the yummm market with a 10-color palette extracted from an image of the food.
 #'
 #' @format
 #' Dataframe of two columns: \describe{
@@ -8,21 +8,39 @@
 #'   \item{Palette}{a list. Palette of colors in hexadecimal format.}
 #' }
 #'
-#' @name yummm.market
+#' @name fill.market
 #'
+#' @import paletter
 #' @import tibble
+#' @import usethis
+#' @import jpeg
+#' @import scales
+#' @export
+
+fill.market <- function(food, n = 10) {
+  pal <- paletter::create_palette(image_path=paste0("inst/images/", food, ".jpg"),
+                                  number_of_colors = n,
+                                  type_of_variable = "categorical")
+
+  pal_df <- tibble::tibble(Col = pal[order(pal)],
+                           ColID = c("01", "02", "03", "04", "05",
+                                     "06", "07", "08", "09", "10"))
+
+  if(file.exists("data/yummm.market.rda")){
+    yummm.market[[food]] <- pal_df
+  } else {
+    yummm.market <- list(pal_df)
+    names(yummm.market) <- food
+  }
+
+  usethis::use_data(yummm.market, overwrite = TRUE)
+}
 
 # Create list of food colors
-yummm.list <- list(
-  banana = "#FFCF4A",
-  avocado = "#332C34",
-  kiwi = "#7FA430",
-  orange = "#FFA210",
-  blueberry = "#4F65AD"
-)
-
-# Create market
-yummm.market <- tibble::tibble(
-  Food = names(yummm.list),
-  Palette = yummm.list
-)
+# yummm.list <- list(
+#   banana = "#FFCF4A",
+#   avocado = "#332C34",
+#   kiwi = "#7FA430",
+#   orange = "#FFA210",
+#   blueberry = "#4F65AD"
+# )
